bb3623d393648b44c3a819ddcd5627de7bf667bf
Bill Ho <ycho.bill.lab@gmail.com>
2025-12-11 00:08:33 +0800

    [Building the Web Layer] (1. Creating the Project, (2. Setting up the Test Class

---
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+
+    @PostMapping
+    public ResponseEntity<String> createTicket() {
+        return new ResponseEntity<>("Hi, TDD", HttpStatus.CREATED);
+    }
+}
---
+++ src/test/java/com/example/demo/controller/TicketControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TicketController.class)
+public class TicketControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(post("/tickets")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+}
